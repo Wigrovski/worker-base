ea34a29dbdb78e17cc8d22f3cd190ad9a02ba9aa
Alexandr <kobal2009@yandex.ru>
2023-01-10 00:53:21 +0500

    Создается объект на основе выбранного класса и добавляется в localStorage + Array

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let surname = document.getElementById('surname')
 let age = document.getElementById('age')
 let exp = document.getElementById('exp')
 let worker = document.getElementById('worker')
+const tableElem = document.getElementById('table')
+const tableData = document.getElementById('table-data')
 let skill = []
 let id = ''
-
+const tableArr = JSON.parse(localStorage.getItem('tableArr')) || []
 
 const btn = document.getElementById('send')
 
@@ -15,6 +17,47 @@ const profArr = {
     driver: ['Права', 'Наличие авто'],
     manager: ['Комуникабельность', 'Грамотность'],
     program: ['Frontend', 'Backend'],
+}
+// Классы сотрудников
+class Worker {
+    constructor(name, surname, age, exp, worker, arr = []) {
+        this.name = name
+        this.surname = surname
+        this.age = age
+        this.exp = exp
+        this.worker = worker
+        this.arr = arr
+    }
+
+}
+class Driver extends Worker {
+    constructor(name, surname, age, exp, worker, arr, lic, auto) {
+        super(name, surname, age, exp, worker, arr)
+        this.lic = lic
+        this.auto = auto
+    }
+    get skill() {return this.lic || this.auto}
+    set skill(value) {this.lic = value ; this.auto = value}
+
+}
+class Manager extends Worker {
+    constructor(name, surname, age, exp, worker, arr, social, literacy) {
+        super(name, surname, age, exp, worker, arr)
+        this.social = social
+        this.literacy = literacy
+    }
+    get skill() {return this.social || this.literacy}
+    set skill(value) {this.social = value ; this.literacy = value}
+}
+class Programmer extends Worker {
+    constructor(name, surname, age, exp, worker, arr, frontend, backend) {
+        super(name, surname, age, exp, worker, arr)
+        this.frontend = frontend
+        this.backend = backend
+    }
+    get skill() {return this.frontend || this.backend}
+    set skill(value) {this.frontend = value ; this.backend = value}
+    
 }
 //Обрабатываем выбор работников
 worker.addEventListener('change', (e) => {
@@ -46,81 +89,9 @@ function createCheckBox(item) {
         prof.appendChild(label)
     }
 }
+// Собираем данные
 
-// Классы сотрудников
-class Worker {
-    constructor(name, surname, age, exp, worker, arr = [] ) {
-        this.name = name
-        this.surname = surname
-        this.age = age
-        this.exp = exp
-        this.worker = worker
-        this.arr = arr
-    }
-    get firstName() { 
-        return  this.arr
-    }
-    set firstName(val) { 
-        this.arr.push(val)
-    }
-
-}
-class Driver extends Worker {
-    constructor(name, surname, age, exp, worker, lic, auto) {
-        super(name, surname, age, exp, worker)
-        this.lic = lic
-        this.auto = auto
-    }
-    get lic() {return this.lic}
-    set auto(value) {this.auto = value}
-}
-class Manager extends Worker {
-    constructor(name, surname, age, exp, worker, social, literacy) {
-        super(name, surname, age, exp, worker)
-        this.social = social
-        this.literacy = literacy
-    }
-    get social() {return this.social}
-    set literacy(value) {this.literacy = value}
-}
-class Programmer extends Worker {
-    constructor(name, surname, age, exp, worker, frontend, backend) {
-        super(name, surname, age, exp, worker)
-        this.frontend = frontend
-        this.backend = backend
-    }
-    get front() {return this.frontend}
-    set front(value) {this.frontend = value}
-    
-}
-// Собираем данные и формируем таблицу
-
-btn.addEventListener('click', () => {
-    dataCol.start()
-
-    if (dataCol.id == 'driver') {
-        const person = new Driver (name, surname, age, exp, worker)
-        person.lic = skill[0]
-        person.auto = skill[1]
-        console.dir(person)
-    }
-    if (dataCol.id == 'manager') {
-        const person = new Manager (name, surname, age, exp, worker)
-        person.social = skill[0]
-        person.literacy = skill[1]
-        console.dir(person)
-    }
-    if (dataCol.id == 'program') {
-        const person = new Manager (name, surname, age, exp, worker)
-        person.front = skill[0]
-        person.backend = skill[1]
-        console.dir(person)
-    }
-    console.log(this.driver);
-})
-
-
-const dataCol = {
+const data = {
     name: '',
     surname: '',
     age: '',
@@ -165,6 +136,40 @@ const dataCol = {
         }
     }
 }
+//Обрабатываем события при нажатии кнопки
+btn.addEventListener('click', () => {
+    data.start()
+
+    if (id == 'driver') {
+        const driver = new Driver (name, surname, age, exp, worker)
+        driver.lic = skill[0]
+        driver.auto = skill[1]
+        driver.arr.push(driver.lic, driver.auto)
+        tableArr.push(driver)
+        console.log(tableArr)
+    }
+    if (id == 'manager') {
+        const manager = new Manager (name, surname, age, exp, worker)
+        manager.social = skill[0]
+        manager.literacy = skill[1]
+        manager.arr.push(manager.social, manager.literacy)
+        tableArr.push(manager)
+        console.log(tableArr)
+    }
+    if (id == 'program') {
+        const programmer = new Programmer (name, surname, age, exp, worker)
+        programmer.frontend = skill[0]
+        programmer.backend = skill[1]
+        programmer.arr.push(programmer.frontend, programmer.backend)
+        tableArr.push(programmer)
+        console.log(tableArr)
+    }
+    localStorage.setItem('tableArr', JSON.stringify(tableArr))
+    
+})
+//Формируем таблицу
+
+
 
 
 
